660f695f57e7ef8bd137cd4a818485a289172d29
zhuyaojie <zyjqjnh@126.com>
2017-06-28 09:55:48 +0800

    editor

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -42,6 +42,6 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="sass" scoped>
+  @import "../assets/sass/pages/Editor.sass";
 </style>
